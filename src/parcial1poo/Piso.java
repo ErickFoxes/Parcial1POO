e6daf1540dc790f2d73a68b3f2d81891cb49595d
Kevin <00139917@uca.edu.sv>
2018-04-30 21:20:49 -0600

    He creado la clase Piso con sus variables y metodos

---
+++ src/parcial1poo/Piso.java
@@ -0,0 +1,18 @@
+package parcial1poo;
+
+/**
+ *
+ * @author Kevin Ariel Vargas Menjivar 00139917
+ */
+
+public class Piso {
+    
+    char letraPiso;
+    boolean mayorCosto;
+    int habitaciones;
+    boolean Disponible;
+    
+    public boolean modEstado(){
+        return true;
+    }    
+}
