e58ed5282bf16c64621055139572d2be85346699
kaztro <>
2018-04-30 18:01:57 -0600

    He creado las clases: ListaReservacion y Costo, sus variables, constructores, setters y getters c:

---
+++ src/parcial1poo/ListaReservacion.java
@@ -0,0 +1,28 @@
+package parcial1poo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kaztro
+ */
+public class ListaReservacion {
+    public ArrayList<String> reservas = new ArrayList<>();
+
+    public ListaReservacion() {
+    }
+    
+    
+    
+    public void agregarReserva(){
+        
+    }
+    
+    public void modificarReserva(){
+        
+    }
+        
+    public void mostrarReserva(){
+        
+    } 
+}
