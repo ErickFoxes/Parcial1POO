abb36edc4cd343d3cb4981ea7a2773599c258f9e
Kevin <00139917@uca.edu.sv>
2018-04-30 21:20:12 -0600

    He creado la clase Habitacion con sus variables y metodos

---
+++ src/parcial1poo/Habitacion.java
@@ -0,0 +1,19 @@
+package parcial1poo;
+
+/**
+ *
+ * @author Kevin Ariel Vargas Menjivar 00139917
+ */
+public class Habitacion {
+    
+    boolean tipo;
+    boolean Disponible;
+    //Costo costo;
+    Piso piso;
+    
+    public boolean HabitacionDisponible(){
+    
+        return true;
+    }
+    
+}
