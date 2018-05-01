4a98f96d4d88a2b7a116d9cbcd4b072857455336
kaztro <kaztro@192.168.1.8>
2018-04-30 22:01:26 -0600

    Comienzo de lista, falta Clase Reserva por eso da error :'v

--- src/parcial1poo/ListaReservacion.java
+++ src/parcial1poo/ListaReservacion.java
@@ -1,21 +1,37 @@
 package parcial1poo;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author kaztro
  */
 public class ListaReservacion {
-    public ArrayList<String> reservas = new ArrayList<>();
+    public ArrayList<String> reservas;
 
-    public ListaReservacion() {
+    public ListaReservacion(ArrayList<String> reservas) {
+        this.reservas = reservas;
     }
-    
-    
-    
+
+    /**
+     * Se utiliza para agregar reservaciones a la lista
+     */
     public void agregarReserva(){
+        Reservacion reservacion = new Reservacion();
+        reservacion.agregarReserva(reservacion);
+        Scanner read = new Scanner(System.in);
+         System.out.println("Ingrese la reservacion");
+        System.out.print(": ");
+        estudiante.setNombres(leer.nextLine());
         
+        System.out.println("Ingrese los apellids del estudiante");
+        System.out.print(": ");
+        estudiante.setApellidos(leer.nextLine());
+        
+        System.out.println("Ingrese el carne del estudiante");
+        System.out.print(": ");
+        estudiante.setCarnet(leer.next());
     }
     
     public void modificarReserva(){
