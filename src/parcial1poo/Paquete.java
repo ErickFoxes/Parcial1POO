585df0d882cc99bccfe5029d91c03bda159ca59f
Erick <00092217@uca.edu.sv>
2018-05-02 11:19:05 -0600

    Se agrego el constructor, los setters y getters

--- src/parcial1poo/Paquete.java
+++ src/parcial1poo/Paquete.java
@@ -11,4 +11,76 @@ package parcial1poo;
  */
 public class Paquete {
     //agregar
+    boolean buffet, piscinaI, piscina,servicioH,minibar,internet;
+    float costoExtra;
+    
+    public Paquete (){ }
+    
+    public Paquete(boolean buffet, boolean piscinaI, boolean piscina, boolean servicioH, boolean minibar, boolean internet, float costoExtra){
+        this.buffet = buffet;
+        this.piscinaI = piscinaI;
+        this.piscina = piscina;
+        this.servicioH = servicioH;
+        this.minibar = minibar;
+        this.internet = internet;
+        this.costoExtra = costoExtra;
+        
+        
+    }
+
+    public boolean getBuffet() {
+        return buffet;
+    }
+
+    public boolean getPiscinaI() {
+        return piscinaI;
+    }
+
+    public boolean getPiscina() {
+        return piscina;
+    }
+
+    public boolean getServicioH() {
+        return servicioH;
+    }
+
+    public boolean getMinibar() {
+        return minibar;
+    }
+
+    public boolean getInternet() {
+        return internet;
+    }
+
+    public float getCostoExtra() {
+        return costoExtra;
+    }
+
+    public void setBuffet(boolean buffet) {
+        this.buffet = buffet;
+    }
+
+    public void setPiscinaI(boolean piscinaI) {
+        this.piscinaI = piscinaI;
+    }
+
+    public void setPiscina(boolean piscina) {
+        this.piscina = piscina;
+    }
+
+    public void setServicioH(boolean servicioH) {
+        this.servicioH = servicioH;
+    }
+
+    public void setMinibar(boolean minibar) {
+        this.minibar = minibar;
+    }
+
+    public void setInternet(boolean internet) {
+        this.internet = internet;
+    }
+
+    public void setCostoExtra(float costoExtra) {
+        this.costoExtra = costoExtra;
+    }
 }
