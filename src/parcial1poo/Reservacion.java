db3fb26a03bc2df2f3bf2fd187fce4788f731f65
Erick <00092217@uca.edu.sv>
2018-05-02 11:22:41 -0600

    Se creó la clase Reservacion y se agrego el constructor, los setters y getters

---
+++ src/parcial1poo/Reservacion.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial1poo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Erick
+ */
+public class Reservacion {
+    int id_Reservacion, diasReservacion;
+    Date fechaReservacion;
+    boolean cancelado;
+    Habitacion habitacion;
+    
+    public Reservacion(){}
+    
+    public Reservacion(int id_Reservacion,Date fechaReservacion, int diasReservacion, boolean cancelado, Habitacion habitacion){
+        this.id_Reservacion = id_Reservacion;
+        this.fechaReservacion = fechaReservacion;
+        this.diasReservacion = diasReservacion;
+        this.cancelado = cancelado;
+        this.habitacion = habitacion;
+        
+    }
+
+    public int getId_Reservacion() {
+        return id_Reservacion;
+    }
+
+    public int getDiasReservacion() {
+        return diasReservacion;
+    }
+
+    public Date getFechaReservacion() {
+        return fechaReservacion;
+    }
+
+    public boolean getCancelado() {
+        return cancelado;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+    
+
+    public void setId_Reservacion(int id_Reservacion) {
+        this.id_Reservacion = id_Reservacion;
+    }
+
+    public void setDiasReservacion(int diasReservacion) {
+        this.diasReservacion = diasReservacion;
+    }
+
+    public void setFechaReservacion(Date fechaReservacion) {
+        this.fechaReservacion = fechaReservacion;
+    }
+
+    public void setCancelado(boolean cancelado) {
+        this.cancelado = cancelado;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+}
