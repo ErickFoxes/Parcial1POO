56be95015c00327936c452b52fc3cf984ff5bcc3
cs <00029613@uca.edu.sv>
2018-05-03 08:29:57 -0600

    Se quitaron los getters y setters, se establecieron las variables booleanas como false

--- src/parcial1poo/Reservacion.java
+++ src/parcial1poo/Reservacion.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class Reservacion {
     int id_Reservacion, diasReservacion;
     Date fechaReservacion;
-    boolean cancelado;
+    boolean cancelado = false;
     Habitacion habitacion;
     
     public Reservacion(){}
@@ -21,46 +21,5 @@ public class Reservacion {
         this.cancelado = cancelado;
         this.habitacion = habitacion;
         
-    }
-
-    public int getId_Reservacion() {
-        return id_Reservacion;
-    }
-
-    public int getDiasReservacion() {
-        return diasReservacion;
-    }
-
-    public Date getFechaReservacion() {
-        return fechaReservacion;
-    }
-
-    public boolean getCancelado() {
-        return cancelado;
-    }
-
-    public Habitacion getHabitacion() {
-        return habitacion;
-    }
-    
-
-    public void setId_Reservacion(int id_Reservacion) {
-        this.id_Reservacion = id_Reservacion;
-    }
-
-    public void setDiasReservacion(int diasReservacion) {
-        this.diasReservacion = diasReservacion;
-    }
-
-    public void setFechaReservacion(Date fechaReservacion) {
-        this.fechaReservacion = fechaReservacion;
-    }
-
-    public void setCancelado(boolean cancelado) {
-        this.cancelado = cancelado;
-    }
-
-    public void setHabitacion(Habitacion habitacion) {
-        this.habitacion = habitacion;
     }
 }
