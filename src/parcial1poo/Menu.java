1d502439ed8dd6fad601ae296ce96974a65a043e
elsyc <00125717@uca.edu.sv>
2018-05-03 17:01:55 -0600

    Se agregaron mas opciones al menu

--- src/parcial1poo/Menu.java
+++ src/parcial1poo/Menu.java
@@ -25,19 +25,21 @@ public class Menu {
         System.out.println("- - - MENÚ - - -");
         System.out.println("1. Ingresar Reservación");
         System.out.println("2. Mostrar Reservaciones (por semana)");
-        System.out.println("3. Modificar Reservación");
-        System.out.println("4. Cancelar Reservación");
-        System.out.println("5. Mantenimiento Piso");
-        System.out.println("6. Mantenimiento Habitacion");
-        System.out.println("7. Modificar Precios de Habitaciónes");
-        System.out.println("8. Modificar paquetes");
-        System.out.println("9. Salir");
+        System.out.println("3. Modificar Reservación (cantidad de dias, fecha y estado)");
+        System.out.println("4. Mantenimiento Piso");
+        System.out.println("5. Mantenimiento Habitacion");
+        System.out.println("6. Modificar Precios de Habitaciónes");
+        System.out.println("7. Agregar paquete");
+        System.out.println("8. Mostrar paquetes");
+        System.out.println("9. Modificar paquetes");
+        System.out.println("10. Salir");
     }
 
     public void menu() {
         int x;
         Scanner M = new Scanner(System.in);
         ListaReservacion lista = new ListaReservacion();
+        ListaPaquetes listaP = new ListaPaquetes();
         escoger();
         try{
             x = M.nextInt();
@@ -52,6 +54,26 @@ public class Menu {
                 case 3:
                     lista.modificarReserva();
                     break;
+                case 4:
+                    System.out.println("Mantenimiento de piso/s");
+                    break;
+                case 5:
+                    System.out.println("Mantenimiento de habitacion/es");
+                    break;
+                case 6:
+                    System.out.println("Modificando precios de habitaciones");
+                    break;
+                case 7:
+                    listaP.AgregarPaquete();
+                    break;
+                case 8:
+                    listaP.MostrarPaquete();
+                    break;
+                case 9:
+                    listaP.ModificarPaquete();
+                    break;
+         
+                    
                 default:
                     System.out.println("Ingresa una opcion entre 1 y 9 por favor...");
             }
