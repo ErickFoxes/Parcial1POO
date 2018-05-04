5cd501f954c3360c54e3c3cce3293e8481a3c554
kevit <00139917@uca.edu.sv>
2018-05-03 19:30:19 -0600

    se crearon los metodos mostrar, agregar y modificar

--- src/parcial1poo/Habitaciones.java
+++ src/parcial1poo/Habitaciones.java
@@ -3,6 +3,7 @@ package parcial1poo;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  *
@@ -12,7 +13,7 @@ public class Habitaciones {
     Map<String,Habitacion> habitaciones;
     
     public Habitaciones(){
-        habitaciones = new HashMap<String,Habitacion>();
+        habitaciones = new HashMap<>();
     }
     
     public void add(String clave,Habitacion habitacion) {
@@ -20,13 +21,47 @@ public class Habitaciones {
     }
     
     public void mostrar(){
-        for(Habitacion h : habitaciones.values()){
+        habitaciones.values().forEach((h) -> {
             System.out.println(h.getPiso());
-        }
+        });
     }
     
     public void modificar(String clave) {
         Habitacion h = habitaciones.get(clave);
-        h.setCosto(3);
+        Scanner s = new Scanner(System.in);
+        int i = s.nextInt();
+        
+        switch (i){
+            case 1:
+                System.out.println("Digite el nuevo costo :");
+                Scanner j = new Scanner(System.in);
+                int costo = j.nextInt();
+                h.setCosto(costo);
+                break;            
+            case 2:
+                System.out.println("Digite el nuevo campo:");
+                Scanner q = new Scanner(System.in);
+                boolean tipo = q.nextBoolean();
+                h.setTipo(tipo);
+                break;
+            case 3:
+                System.out.println("Digite el nuevo campo:");
+                Scanner k = new Scanner(System.in);
+                boolean disponible = k.nextBoolean();
+                h.setDisponible(disponible);
+                break;
+            case 4:
+                System.out.println("Digite el nuevo piso:");
+                Scanner o = new Scanner(System.in);
+                String piso = o.nextLine();
+                h.setPiso(piso);
+                break;
+            case 5:
+                System.out.println("Digite el nuevo numero de habitacion:");
+                Scanner l = new Scanner(System.in);
+                int numHb = l.nextInt();
+                h.setNumHb(numHb);
+                break;
+        }
     }
 }
