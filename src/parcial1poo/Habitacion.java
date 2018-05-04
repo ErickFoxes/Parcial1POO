d270b32945a02d77f74a4b597f7a3c36775cafaf
kevit <00139917@uca.edu.sv>
2018-05-03 18:58:33 -0600

    Se borraron los metodos

--- src/parcial1poo/Habitacion.java
+++ src/parcial1poo/Habitacion.java
@@ -1,24 +1,30 @@
 package parcial1poo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Kevin Ariel Vargas Menjivar 00139917
  */
 public class Habitacion {
 
-    boolean tipo;
-    boolean Disponible = true;
-    Costo costo;
-    Piso piso;
+    private boolean tipo;
+    private boolean Disponible = true;
+    private double costo;
+    private String piso;
+    private int numHb;
     
-    public void Habitaciones(boolean tipo, boolean Disponible, Costo costo, Piso piso){
+    public Habitacion(){
+    }
+    
+    public Habitacion(boolean tipo, boolean Disponible, double costo, String piso, int numHb){
         this.tipo = tipo;
         this.Disponible = Disponible;
         this.costo = costo;
         this.piso = piso;
+        this.numHb = numHb;
+    } 
     
-    }
-
     public boolean VerificarEstado(){
         return Disponible;
     }
@@ -26,6 +32,47 @@ public class Habitacion {
     public boolean ModificarEstado() {
             return !Disponible;
     }
+
+    public boolean isTipo() {
+        return tipo;
+    }
+
+    public void setTipo(boolean tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isDisponible() {
+        return Disponible;
+    }
+
+    public void setDisponible(boolean Disponible) {
+        this.Disponible = Disponible;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    public int getNumHb() {
+        return numHb;
+    }
+
+    public void setNumHb(int numHb) {
+        this.numHb = numHb;
+    }
+    
     
     
 }
