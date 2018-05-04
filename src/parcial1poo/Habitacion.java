df1a29ee088d4366a24f2c8bdbc87a8b81eea6db
Erick <00092217@uca.edu.sv>
2018-05-04 09:01:46 -0600

    Se agrego el idHabitacion

--- src/parcial1poo/Habitacion.java
+++ src/parcial1poo/Habitacion.java
@@ -7,23 +7,31 @@ import java.util.ArrayList;
  * @author Kevin Ariel Vargas Menjivar 00139917
  */
 public class Habitacion {
-
     private boolean tipo;
     private boolean Disponible = true;
     private double costo;
-    private String piso;
+    private String piso,idHabitacion;
     private int numHb;
     
     public Habitacion(){
     }
     
-    public Habitacion(boolean tipo, boolean Disponible, double costo, String piso, int numHb){
+    public Habitacion(String idHabitacion,boolean tipo, boolean Disponible, double costo, String piso, int numHb){
         this.tipo = tipo;
         this.Disponible = Disponible;
         this.costo = costo;
         this.piso = piso;
+        this.idHabitacion = idHabitacion;
         this.numHb = numHb;
     } 
+
+    public String getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(String idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
     
     public boolean VerificarEstado(){
         return Disponible;
