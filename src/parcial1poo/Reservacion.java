6b5b83799957ff05378199ac55a9cd3402a0db9e
Erick <00092217@uca.edu.sv>
2018-05-08 03:39:00 -0600

    cambios menores

--- src/parcial1poo/Reservacion.java
+++ src/parcial1poo/Reservacion.java
@@ -8,18 +8,19 @@ public class Reservacion {
     int id_Reservacion, diasReservacion;
     Fecha fechaReservacion;
     boolean cancelado = false;
-    Habitacion habitacion;
+    String habitacion;
     Cliente cliente;
+    double costo;
     
     public Reservacion(){}
     
-    public Reservacion(int id_Reservacion,Fecha fechaReservacion, int diasReservacion, boolean cancelado, Habitacion habitacion){
+    public Reservacion(int id_Reservacion,Fecha fechaReservacion, int diasReservacion, boolean cancelado, String habitacion,double costo){
         this.id_Reservacion = id_Reservacion;
         this.fechaReservacion = fechaReservacion;
         this.diasReservacion = diasReservacion;
         this.cancelado = cancelado;
         this.habitacion = habitacion;
-        
+        this.costo = costo;
     }
     
     public String vigente(){
@@ -34,7 +35,7 @@ public class Reservacion {
     @Override
     public String toString() {
         return "**Reservacion** \n"+">>El id es: "+id_Reservacion+" \n>>Cantidad de dias: "+diasReservacion+"\n>>Fecha reservacion: "+fechaReservacion+"\n>>Cliente: \n"+cliente
-                +"\n>>Estado:"+ vigente()+"\n";
+                +"\n>>Estado:"+ vigente()+"\nCosto total:"+costo;
 
     }
 }
